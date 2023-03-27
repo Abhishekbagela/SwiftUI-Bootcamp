f9f52687f3d111fc295d061448c88807b1aab990
Abhishek-ab@ios-dev <abhibagela2015@gmail.com>
2023-03-27 23:08:14 +0530

    Initial Commit

---
+++ SwiftUI-Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI-Bootcamp
+//
+//  Created by Abhishek Bagela on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
